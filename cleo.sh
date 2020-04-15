ec251b86a2783db0d37db7ea0076820fea07fee9
CleoCyber21 <63235670+CleoCyber21@users.noreply.github.com>
2020-04-15 13:05:08 +0700

    Add files via upload

---
+++ cleo.sh
@@ -0,0 +1,252 @@
+#usr/bin/bash
+clear
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+#Mr.CLEOCYBER_
+echo
+echo $i"["$bi"•"$i"]"$me"───────────────────────────────────────────"$i"["$bi"•"$i"]"
+echo $i" |"$ku"                  HACK FB"$ku"                    |"
+echo $i"["$bi"•"$i"]"$me"───────────────────────────────────────────"$i"["$bi"•"$i"]"
+echo $i" |"$me" 1"$i" |"$cy"   Brute force FACEBOOK ~ BR1G4DE ~      "$i"|"$i"
+echo $i" |"$me" 2"$i" |"$cy"   Brute force FACEBOOK ~Nightmare~      "$i"|"$i"
+echo $i" |"$me" 3"$i" |"$cy"   Brute force FACEBOOK ~   MBF   ~      "$i"|"$i"
+echo $i" |"$me" 4"$i" |"$cy"   Brute force FACEBOOK  ( Root )        "$i"|"$i"
+echo $i"["$bi"•"$i"]"$me"───────────────────────────────────────────"$i"["$bi"•"$i"]"
+echo
+echo
+echo $i"["$bi"•"$i"]"$me"───────────────────────────────────────────"$i"["$bi"•"$i"]"
+echo $i" |"$ku"                  BOOSTER FB                 |"
+echo $i"["$bi"•"$i"]"$me"───────────────────────────────────────────"$i"["$bi"•"$i"]"
+echo $i" |"$me" 5"$i" |"$cy"   Information   FACEBOOK                "$i"|"$i"
+echo $i" |"$me" 6"$i" |"$cy"   Profil GUARD  FACEBOOK                "$i"|"$i"
+echo $i" |"$me" 7"$i" |"$cy"   Auto Report   FACEBOOK                "$i"|"$i"
+echo $i" |"$me" 8"$i" |"$cy"   Auto LIKE     FACEBOOK                "$i"|"$i"
+echo $i" |"$me" 9"$i" |"$cy"   Auto reaction FACEBOOK                "$i"|"$i"
+echo $i" |"$me"10"$i" |"$cy"   yahoo Clone   FACEBOOK                "$i"|"$i"
+echo $i" |"$me"11"$i" |"$cy"   Bot komentar  FACEBOOK                "$i"|"$i"
+echo $i"["$bi"•"$i"]"$me"───────────────────────────────────────────"$i"["$bi"•"$i"]
+echo
+echo
+echo $i"["$bi"•"$i"]"$me"───────────────────────────────────────────"$i"["$bi"•"$i"]
+echo $i"|"$cy"                  PHISING FB                 |"
+echo $i"["$bi"•"$i"]"$me"───────────────────────────────────────────"$i"["$bi"•"$i"]
+echo $i"|"$me"12"$i" |"$cy"    PHISING v1   FACEBOOK                "$i"|"$i"
+echo $i"|"$me"13"$i" |"$cy"    PHISING v2   FACEBOOK                "$i"|"$i"
+echo $i"|"$me"14"$i" |"$cy"    PHISING v3   FACEBOOK                "$i"|"$i"
+echo $i"|"$me"15"$i" |"$cy"    PHISING v4   FACEBOOK                "$i"|"$i"
+echo $i"["$bi"•"$i"]"$me"───────────────────────────────────────────"$i"["$bi"•"$i"]
+echo
+echo
+echo $i"["$bi"•"$i"]"$me"───────────────────────────────────────────"$i"["$bi"•"$i"]
+echo $i"|"$"                  PHISING FB                 |
+echo $i"["$bi"•"$i"]"$me"───────────────────────────────────────────"$i"["$bi"•"$i"]
+echo $i"|"$me"16"$i" |"$cy"    INSTAL BAHAN SEMUA CLEO TOOLS        "$i"|"$i"
+echo $i"|"$me"17"$i" |"$cy"    EXIT PROGRAM CLEO TOOLS              "$i"|"$i"
+echo $i"["$bi"•"$i"]"$me"───────────────────────────────────────────"$i"["$bi"•"$i"]
+echo
+echo $ku"┲┅ [ pilih nomornya ] ┅┲
+echo $ku"┋"
+echo -p"┖┅# " pil
+
+read -p" " pil
+
+if [ $pil = 1 ]
+then
+  clear
+figlet -f slant "OPEN CLEO TOOLS"|lolcat
+sleep 1
+git clone https://github.com/FR13ND8/Fb-cracker-v.3
+cd Fb-cracker-v.3
+ls
+python2 crack.py
+fi
+
+if [ $pil = 2 ]
+then
+  clear
+figlet -f slant "OPEN CLEO TOOLS"|lolcat
+sleep 1
+git clone https://github.com/N1ght420/FaceBrute
+cd FaceBrute
+python fb.py
+fi
+
+if [ $pil = 3 ]
+then
+  clear
+figlet -f slant "OPEN CLEO TOOLS"|lolcat
+sleep 1
+git clone https://github.com/FR13ND8/mbf
+cd mbf
+python2 MBF.py
+fi
+
+if [ $pil = 4  ]
+ then
+  clear
+figlet -f slant "OPEN CLEO TOOLS"|lolcat
+sleep 1
+git clone https://github.com/thelinuxchoice/facebash.git
+cd facebash
+bash facebash.sh
+fi
+
+if [ $pil = 5 ]
+then
+  clear
+figlet -f slant "OPEN CLEO TOOLS"|lolcat
+sleep 1
+git clone https://github.com/FR13ND8/InfoFB
+cd InfoFB
+pip2 install -r requirements.txt
+python2 info.py
+fi
+
+if [ $pil = 6 ]
+then
+  clear
+figlet -f slant "OPEN CLEO TOOLS"|lolcat
+sleep 1
+git clone https://github.com/FR13ND8/ProfileGuardFb
+cd ProfileGuardFb
+php guard.php
+fi
+
+if [ $pil = 7 ]
+then
+  clear
+figlet -f slant "OPEN CLEO TOOLS"|lolcat
+sleep 1
+git clone https://github.com/IlayTamvan/Report
+cd Report
+unzip Report.unzip
+python2 Report.py
+fi
+
+if [ $pil = 8 ]
+then
+  clear
+figlet -f slant "OPEN CELO TOOLS"|lolcat
+sleep 1
+git clone https://github.com/FR13ND8/autolike
+cd autolike
+php autolike.php
+fi
+
+if [ $pil = 9 ]
+then
+  clear
+figlet -f slant "OPEN CLEO TOOLS"|lolcat
+sleep 1
+git clone https://github.com/AMVengeance/FB-reaction
+cd FB-React
+./start
+fi
+
+if [ $pil = 10 ]
+then
+  clear
+figlet -f slant "OPEN CLEO TOOLS"|lolcat
+sleep 1
+git clone https://github.com/wahyuandhika/YahooCloning
+cd YahooCloning
+ls
+pip2 install request mechanize
+pip2 install requirements
+python2 cloning.py
+fi
+
+if [ $pil = 11 ]
+then
+  clear
+figlet -f slant "OPEN CLEO TOOLS"|lolcat
+sleep 1
+git clone https://github.com/Senitopeng/Botkomena.git
+cd Botkomena
+python2 botkomena.py
+fi
+
+if [ $pil = 12 ]
+then
+  clear
+figlet -f slant "OPEN CLEO TOOLS"|lolcat
+sleep 1
+git clone https://github.com/thelinuxchoice/shellphish.git
+cd shellphish
+bash shellphish.sh
+fi
+
+if [ $pil = 13 ]
+then
+  clear
+figlet -f slant "OPEN CLEO TOOLS"|lolcat
+sleep 1
+git clone https://github.com/thelinuxchoice/blackeye.git
+cd blackeye
+bash blackeye.sh
+fi
+
+if [ $pil = 14 ]
+then
+  clear
+figlet -f slant "OPEN CLEO TOOLS"|lolcat
+sleep 1
+git clone https://github.com/UndeadSec/SocialFish.git
+cd SocialFish
+chmod +x *
+pip2 install -r requirements.txt
+python2 SocialFish.py
+fi
+
+if [ $pil = 15 ]
+then
+  clear
+figlet -f slant "OPEN CLEO TOOLS"|lolcat
+sleep 1
+git clone https://github.com/evait-security/weeman.git
+cd weeman
+chmod +x *
+python2 weeman.py
+fi
+
+if [ $pil = 16]
+then
+  clear
+  apt update && apt upgrade
+  apt install python2
+  pip2 install urllib3 chardet certifi idna requests
+  pkg install git
+  pip2 install mechanize
+  pkg install curl
+  pkg install python
+  pkg install ruby
+  pkg install gem
+  gem install lolcat
+  pkg instal figlet
+  pkg install php
+  pkg install toilet
+  pkg install cowsay
+  pkg install neofetch
+  pkg install nano
+figlet -f slant "S U K S E S"|lolcat
+fi
+
+if [ $pil = 17 ]
+then
+  clear
+figlet -f slant " E X I T"|lolcat
+sleep 2
+echo $pu"Terima kasih sudah menggunakan tools CLEO CYBER"
+sleep 2
+echo $pu"jangan lupa ikuti saya"
+sleep 2
+echo $ku"INSTAGRAM :"$i" CLEOCYBER_ "
+echo $ku"   YT     :"$i" CLEO CYBER "
+sleep 3
+exit
+fi
